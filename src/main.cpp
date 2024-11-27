0a7fc60901cf00d19c37f2c3ed9f497a1e8e0ac9
artem <artem-romanyuk-03@mail.ru>
2024-11-27 18:21:04 +0300

    посылка данных по UART в последовательный порт

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,3 @@
-
-//#include <libopencm3/cm3/nvic.h>
-//#include <libopencm3/stm32/rcc.h>
-//#include <libopencm3/stm32/gpio.h>
-//#include <libopencm3/stm32/timer.h>
-//#include <libopencm3/stm32/usart.h>
-//#include <stdio.h>
-
 #include <libopencm3/stm32/f4/rcc.h>
 #include <libopencm3/stm32/f4/gpio.h>
 #include <libopencm3/stm32/f4/timer.h>
@@ -23,25 +15,43 @@ void tim2_isr(void) {
 }
 
 void usart_setup(void) {
-    rcc_peripheral_enable_clock(&RCC_APB1ENR, RCC_USART2);
-    rcc_peripheral_enable_clock(&RCC_AHB1ENR, RCC_GPIOE);
+    // Включаем тактирование для GPIOA и USART2
+    rcc_periph_clock_enable(RCC_GPIOA);
+    rcc_periph_clock_enable(RCC_USART2);
 
-    // Настройка PA2 как альтернативной функции (USART2_TX)
-    gpio_mode_setup(GPIOA, GPIO_MODE_AF, GPIO_PUPD_NONE, GPIO2);
-    gpio_set_af(GPIOA, GPIO_AF7, GPIO2); // USART2_TX
+    // Настраиваем пины PA2 (TX) и PA3 (RX) для USART2
+    gpio_mode_setup(GPIOA, GPIO_MODE_AF, GPIO_PUPD_NONE, GPIO2 | GPIO3);
+    gpio_set_af(GPIOA, GPIO_AF7, GPIO2); // TX
+    gpio_set_af(GPIOA, GPIO_AF7, GPIO3); // RX
 
-    // Настройка USART
-    USART_CR1(USART2) &= ~USART_CR1_UE; // Отключаем USART
-    USART_BRR(USART2) = 115200; // Настройка скорости 115200
-    USART_CR1(USART2) |= USART_CR1_TE; // Включаем передатчик
-    USART_CR1(USART2) |= USART_CR1_UE; // Включаем USART
+    // Настраиваем USART2
+    usart_set_baudrate(USART2, 115200); // Устанавливаем скорость бит/с 
+    usart_set_databits(USART2, 8); // 8 бит данных
+    usart_set_stopbits(USART2, USART_STOPBITS_1); // 1 стоп-бит
+    usart_set_mode(USART2, USART_MODE_TX_RX); // Режим TX и RX
+    usart_set_parity(USART2, USART_PARITY_NONE); // Без четности
+    usart_set_flow_control(USART2, USART_FLOWCONTROL_NONE); // Без управления потоком
+
+    usart_enable_rx_interrupt(USART2);
+    nvic_enable_irq(NVIC_USART2_IRQ);
+    
+    // Включаем USART2
+    usart_enable(USART2);
 }
 
 void usart_send_string(const char *str) {
     while (*str) {
         while (!(USART_SR(USART2) & USART_SR_TXE)); // Ждем, пока TXE не станет 1
-        USART_DR(USART2) = *str++;  ///это отправка в юсарт можно ее использовать 
+        USART_DR(USART2) = *str++;  // Отправка символа
     }
+
+    gpio_toggle(GPIOD, GPIO15); // Настройка мигания
+}
+
+// Инициализация всех светодиодов 
+void setup_LED(void){
+    rcc_periph_clock_enable(RCC_GPIOD);
+    gpio_mode_setup(GPIOD, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO15 | GPIO14 | GPIO13 | GPIO12);
 }
 
 void setup_timer(void) {
@@ -49,6 +59,7 @@ void setup_timer(void) {
     rcc_peripheral_enable_clock(&RCC_AHB1ENR, RCC_GPIOE);
 
     gpio_mode_setup(GPIOA, GPIO_MODE_AF, GPIO_PUPD_NONE, GPIO0);
+    
     gpio_set_af(GPIOA, GPIO_AF1, GPIO0); // TIM2_CH1
 
     TIM_CR1(TIM2) &= ~TIM_CR1_CEN; // Отключаем таймер
@@ -67,15 +78,15 @@ void setup_timer(void) {
 int main(void) {
     usart_setup(); // Настройка UART
     setup_timer(); // Настройка таймера
+    setup_LED();   // Инициализация светодиодов
 
-    while (1) {    ////дописать включение светодиода   
-        char buffer[50];
-        // Преобразуем pulse_width в строку и отправляем по UART
-        snprintf(buffer, sizeof(buffer), "Pulse Width: %lu\n", pulse_width);
-        usart_send_string(buffer);  /////вместо этого написать отправку числа 85 в юсарт 
+    while (1) {
+        char number_buffer[20];
+        snprintf(number_buffer, sizeof(number_buffer), "Number: %d\n", 85); // Форматируем число в строку
         
-        // Добавим небольшую задержку, чтобы избежать слишком частого вывода
-        for (volatile int i = 0; i < 100000; i++);
+        usart_send_string(number_buffer);  // Отправка числа 85 в виде строки
+        
+        for (volatile int i = 0; i < 100000; i++); // Задержка
     }
 
     return 0;
